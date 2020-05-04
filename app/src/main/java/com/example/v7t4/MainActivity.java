ca27bae766c6ef29c07198de589540b409e60377
Juha <juha1lahteinen@gmail.com>
2020-05-04 20:19:28 +0300

    Viikko 7 tehtävä 4

---
+++ app/src/main/java/com/example/v7t4/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.v7t4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textout;
+    EditText input;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        input = (EditText) findViewById(R.id.editText);
+        textout = (TextView) findViewById(R.id.textView);
+
+        input.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String name = input.getText().toString();
+                textout.setText(name);
+            }
+        });
+
+    }
+}
